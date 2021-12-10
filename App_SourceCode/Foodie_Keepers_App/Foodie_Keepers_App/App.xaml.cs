a6382b4c64b86936d6c7261076f3060316327af1
Ediwna <55391031+Ediwna@users.noreply.github.com>
2021-12-10 14:01:49 +0900

    Add files via upload

---
+++ App_SourceCode/Foodie_Keepers_App/Foodie_Keepers_App/App.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Diagnostics;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Foodie_Keepers_App.View;
+
+namespace Foodie_Keepers_App
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            // MainPage = new MainPage();
+            MainPage = new NavigationPage(new EntryPage());
+
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+            Console.WriteLine("App start");
+            Debug.WriteLine("App starts");
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+            Console.WriteLine("App sleep");
+            Debug.WriteLine("App sleeps");
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+            Console.WriteLine("App resume");
+            Debug.WriteLine("App resumes");
+        }
+    }
+}
---
+++ App_SourceCode/Foodie_Keepers_App/Foodie_Keepers_App/Models/Users.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Foodie_Keepers_App.Models
+{
+    public class Users
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
